726712604ed4cac3e821ca28cef1efacec00a85e
Alex Fructo <alexfructo@gmail.com>
2020-07-11 12:58:00 -0300

    Implementado a filtragem dos resultados da query equipamentos no método assets_list_as_json.

--- core/views.py
+++ core/views.py
@@ -5,7 +5,7 @@ from django.contrib import messages
 from django.contrib.auth import login, logout
 from django.contrib.auth.decorators import login_required
 from .forms import LoginForm, EquipamentoForm
-from .models import Equipamento
+from .models import Equipamento, Localizacao, Setor, Grupo, Categoria, Fabricante
 
 
 def user_login(request):
@@ -41,15 +41,47 @@ def error_404(request, message):
 
 @login_required()
 def assets(request):
-    return render(request, 'assets.html')
+    localizacao = Localizacao.objects.all()
+    setor = Setor.objects.all()
+    grupo = Grupo.objects.all()
+    categoria = Categoria.objects.all()
+    fabricante = Fabricante.objects.all()
+    context = {
+        "localizacao": localizacao,
+        "setor": setor,
+        "grupo": grupo,
+        "categoria": categoria,
+        "fabricante": fabricante,
+    }
+    return render(request, 'assets.html', context)
 
 
 @login_required()
 def assets_list_as_json(request):
-    equipamentos = Equipamento.objects.all()
-    data = [equipamento.listar_json() for equipamento in equipamentos]
-    response = {'data': data}
-    return JsonResponse(response, safe=False)
+    if request.method == 'POST':
+        equipamentos = Equipamento.objects.all()
+        if request.POST.get('localizacao'):
+            equipamentos = equipamentos.filter(
+                localizacao=request.POST.get('localizacao'))
+        if request.POST.get('setor'):
+            equipamentos = equipamentos.filter(setor=request.POST.get('setor'))
+        if request.POST.get('grupo'):
+            equipamentos = equipamentos.filter(grupo=request.POST.get('grupo'))
+        if request.POST.get('categoria'):
+            equipamentos = equipamentos.filter(
+                categoria=request.POST.get('categoria'))
+        if request.POST.get('status'):
+            equipamentos = equipamentos.filter(
+                status=request.POST.get('status'))
+
+        data = [equipamento.listar_json() for equipamento in equipamentos]
+        response = {'data': data}
+        return JsonResponse(response, safe=False)
+    else:
+        equipamentos = Equipamento.objects.all()
+        data = [equipamento.listar_json() for equipamento in equipamentos]
+        response = {'data': data}
+        return JsonResponse(response, safe=False)
 
 
 @login_required()
@@ -62,6 +94,7 @@ def asset_register(request):
             message = 'Cadastro realizado com sucesso!'
             return redirect(asset_edit(request, codigo=codigo, message=message))
     return render(request, 'asset_register.html', {"form": form})
+
 
 @login_required()
 def asset_edit(request, codigo, message=None):
