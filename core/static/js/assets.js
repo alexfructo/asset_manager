ebec2349041b1af916b9aaddbc91f17e22182448
Alex Fructo <alexfructo@gmail.com>
2020-07-12 21:27:09 -0300

    Alteração no ajax do Datatable, adicionado o dado fabricante.

--- core/static/js/assets.js
+++ core/static/js/assets.js
@@ -1,6 +1,9 @@
 
-function getData(filter, _callback) {
+function getData() {
     return $('#assets').DataTable({
+        "language": {
+            "url": "http://cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese-Brasil.json",
+        },
         "ajax": {
             "url": "/equipamentos/listar/json",
             "type": "POST",
@@ -9,19 +12,18 @@ function getData(filter, _callback) {
                 "setor": function() { return $('#setor').val() },
                 "grupo": function() { return $('#localizacao').val() },
                 "categoria": function() { return $('#equipamento').val() },
+                "fabricante": function() { return $('#fabricante').val() },
                 "status": function() { return $('#status').val() },
                 "csrfmiddlewaretoken": function() { return $("input[name='csrfmiddlewaretoken']").val() },
             }
         },
+        "processing": true,
         responsive: true,
         "bLengthChange": false,
         columnDefs: [
             { responsivePriority: 1, targets: 0 },
             { responsivePriority: 2, targets: -1 }
         ],
-        "language": {
-            "url": "http://cdn.datatables.net/plug-ins/1.10.21/i18n/Portuguese-Brasil.json"
-        },
         "columns": [
             { "data": "localizacao" },
             { "data": "setor" },
@@ -38,8 +40,7 @@ function getData(filter, _callback) {
                     result = "<a href='/equipamento/editar/" + data + "' class='btn btn-icon btn-pill btn-primary btn-sm' data-toggle='tooltip' title='Editar'><i class='fa fa-fw fa-edit'></i> </a> <a href='/equipamento/remover/" + data + "' class='btn btn-icon btn-pill btn-danger btn-sm' data-toggle='tooltip' title='Remover'> <i class='fa fa-fw fa-trash'></i></a>";
                     return result
                 }
-            }
-        ]
+            },
+        ],
     });
-    _callback();
 }
